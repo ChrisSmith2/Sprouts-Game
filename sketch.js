c049254d95910eb5d30676e2fc2f8482e8fc4cd2
Chris5407 <christopher.smith4202@gmail.com>
2019-09-01 10:41:13 -0500

    Dots can now only be placed on lines

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+let maxLinesPerDot = 3;
+let startingDots = 2;
+
 let dots = [];
 let dotRadius = 15;
 let lines = [];
@@ -61,7 +64,7 @@ function mouseDragged() {
 }
 
 function mouseReleased() {
-  if (!dotsPlaced && dots.length >= 2 && waitingDotRelease) {
+  if (!dotsPlaced && dots.length >= startingDots && waitingDotRelease) {
     dotsPlaced = true;
     waitingDotRelease = false;
   }
@@ -86,12 +89,17 @@ function mouseReleased() {
 
 function mousePressed() {
   if (!dotsPlaced) {
-    if (dots.length < 2) {
+    if (dots.length < startingDots) {
       dots.push(new Dot(mouseX, mouseY));
       waitingDotRelease = true;
     } else {
-      dots.push(new Dot(mouseX, mouseY));
-      waitingDotRelease = true;
+      if (closeToLine(mouseX, mouseY, lines[lines.length-1])) {
+        dots.push(new Dot(mouseX, mouseY, true));
+        waitingDotRelease = true;
+        console.log(dots);
+      } else {
+        console.log("off line");
+      }
     }
   }
 }
@@ -99,7 +107,7 @@ function mousePressed() {
 function insideOpenDot() {
   for (let i = 0; i < dots.length; i++) {
     if (dist(dots[i].x, dots[i].y, mouseX, mouseY) <= dotRadius) {
-      if (dots[i].lineCount < 3)
+      if (dots[i].lineCount < maxLinesPerDot)
         return dots[i];
       return null;
     }
@@ -109,13 +117,17 @@ function insideOpenDot() {
 
 function cancelCurrentLine() {
     lines.pop();
-    currentLine = false;
+    currentLine = null;
 }
 
-function Dot(x, y) {
+function Dot(x, y, onLine) {
   this.x = x;
   this.y = y;
-  this.lineCount = 0;
+
+  if (onLine)
+    this.lineCount = 2;
+  else
+    this.lineCount = 0;
 }
 
 function Line(startDot) {
@@ -144,7 +156,7 @@ function intersect(ls1, ls2) {
   let a = ls1.x1, b = ls1.y1, c = ls1.x2, d = ls1.y2;
   let p = ls2.x1, q = ls2.y1, r = ls2.x2, s = ls2.y2;
   
-  // from https://stackoverflow.com/a/24392281
+  // https://stackoverflow.com/a/24392281
   var det, gamma, lambda;
   det = (c - a) * (s - q) - (r - p) * (d - b);
   if (det === 0) {
@@ -154,4 +166,42 @@ function intersect(ls1, ls2) {
     gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
     return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
   }
+}
+
+function closeToLine(mouseX, mouseY, line) {
+  for (var i = 0; i < line.lineSegs.length; i++) {
+    if (isOnLineWithEndCaps(mouseX, mouseY, line.lineSegs[i], 5)) {
+      console.log('good')
+      return true;
+    }
+  }
+  return false;
+}
+
+function isOnLineWithEndCaps (xp, yp, ls, maxDistance) {
+    // https://stackoverflow.com/a/34474547
+    let x1 = ls.x1, y1 = ls.y1, x2 = ls.x2, y2 = ls.y2;
+
+    var dxL = x2 - x1, dyL = y2 - y1;  // line: vector from (x1,y1) to (x2,y2)
+    var dxP = xp - x1, dyP = yp - y1;  // point: vector from (x1,y1) to (xp,yp)
+    var dxQ = xp - x2, dyQ = yp - y2;  // extra: vector from (x2,y2) to (xp,yp)
+
+    var squareLen = dxL * dxL + dyL * dyL;  // squared length of line
+    var dotProd   = dxP * dxL + dyP * dyL;  // squared distance of point from (x1,y1) along line
+    var crossProd = dyP * dxL - dxP * dyL;  // area of parallelogram defined by line and point
+
+    // perpendicular distance of point from line
+    var distance = Math.abs(crossProd) / Math.sqrt(squareLen);
+
+    // distance of (xp,yp) from (x1,y1) and (x2,y2)
+    var distFromEnd1 = Math.sqrt(dxP * dxP + dyP * dyP);
+    var distFromEnd2 = Math.sqrt(dxQ * dxQ + dyQ * dyQ);
+
+    // if the point lies beyond the ends of the line, check if
+    // it's within maxDistance of the closest end point
+    if (dotProd < 0) return distFromEnd1 <= maxDistance;
+    if (dotProd > squareLen) return distFromEnd2 <= maxDistance;
+
+    // else check if it's within maxDistance of the line
+    return distance <= maxDistance;
 }
